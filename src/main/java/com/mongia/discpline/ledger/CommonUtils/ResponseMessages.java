31bbb070924e9dcbc3664636d923634a941c90b8
Shubham Mongia <mon789gia@gmail.com>
2025-05-15 00:59:22 +0530

    add validations and response messages file

---
+++ src/main/java/com/mongia/discpline/ledger/CommonUtils/ResponseMessages.java
@@ -0,0 +1,10 @@
+package com.mongia.discpline.ledger.CommonUtils;
+
+public class ResponseMessages {
+
+    public static final String EXPENSE_CREATED = "Expense created successfully";
+    public static final String EXPENSE_UDPATED = "Expense updated successfully";
+
+    public static final String EXPENSE_DELETED = "Expense deleted successfully";
+    public static final String EXPENSE_FETCHED="Get expenses";
+}
--- src/main/java/com/mongia/discpline/ledger/DTOs/ExpenseRequest.java
+++ src/main/java/com/mongia/discpline/ledger/DTOs/ExpenseRequest.java
@@ -1,5 +1,6 @@
 package com.mongia.discpline.ledger.DTOs;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +10,16 @@ import lombok.Setter;
 @Setter
 public class ExpenseRequest {
 
+    @NotBlank(message = "category cannot be empty")
+    @Size(min=1, max=255,message = "Category size should be between 1 to 255")
     private String category;
+
+    @NotNull
+    @Min(value=0, message ="should be greater than zero" )
+    @Max(value = 100000,message = "Should be less than a lakh")
     private Double amount;
+
+    @NotBlank(message = "Description cannot be empty")
+    @Size(min=1, max=255,message = "Description size should be between 1 to 255")
     private String description;
 }
