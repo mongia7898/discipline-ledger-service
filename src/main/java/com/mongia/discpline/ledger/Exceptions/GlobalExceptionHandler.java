ceab94db63c4e7e7341f98f8164fc47d9c64358f
Shubham Mongia <mon789gia@gmail.com>
2025-05-15 01:13:28 +0530

    push list of string as invalid exception

--- src/main/java/com/mongia/discpline/ledger/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/mongia/discpline/ledger/Exceptions/GlobalExceptionHandler.java
@@ -8,24 +8,27 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        StringBuilder strBuilder = new StringBuilder();
+        List<String> errorMessages = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> {
+                    String fieldName;
+                    try {
+                        fieldName = ((FieldError) error).getField();
+                    } catch (ClassCastException ex) {
+                        fieldName = error.getObjectName();
+                    }
+                    return String.format("%s: %s", fieldName, error.getDefaultMessage());
+                })
+                .collect(Collectors.toList());
 
-        e.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName;
-            try {
-                fieldName = ((FieldError) error).getField();
-
-            } catch (ClassCastException ex) {
-                fieldName = error.getObjectName();
-            }
-            String message = error.getDefaultMessage();
-            strBuilder.append(String.format("%s: %s ,", fieldName, message));
-        });
-
-        return new ResponseEntity<>(new LedgerResponse<String>(false,strBuilder.toString()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new LedgerResponse<List<String>>(false,"Failed",errorMessages), HttpStatus.BAD_REQUEST);
     }
 }
